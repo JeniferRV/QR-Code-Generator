39d591f659e77bd7cbc24361d094872a22086b42
Jenifer <148918882+JeniferRV@users.noreply.github.com>
2024-06-03 14:58:24 -0400

    Update generate_qr.py

--- generate_qr.py
+++ generate_qr.py
@@ -1,12 +1,12 @@
 import qrcode
 from io import BytesIO
 
-def generate_qr_code(link):
+def generate_qr_code(link, color):
     qr = qrcode.QRCode(version=1, box_size=10, border=5)
     qr.add_data(link)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", back_color="white")
+    img = qr.make_image(fill_color=color, back_color="white") # Use color variable
     buffer = BytesIO()
     img.save(buffer, format='PNG')
     buffer.seek(0)
-    return buffer
+    return buffer.getvalue() # Return base64 encoded image
